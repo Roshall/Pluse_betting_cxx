5747c1a9534d6d50c42b1df85a81d147c7b4306d
Roshall <lg6zi7ran6@gmail.com>
2026-05-19 22:24:17 +0800

    Refactor betting factory to support type-erased strategies and improve return type consistency

--- include/betting_by_time/framework.hpp
+++ include/betting_by_time/framework.hpp
@@ -6,6 +6,7 @@
 #include "betting_by_time/capital/sequence_checking.hpp"
 #include "betting_by_time/strategies/vanilla_betting.hpp"
 #include "betting_by_time/strategies/adaptive_betting.hpp"
+#include <any>
 #include <functional>
 #include <memory>
 #include <stdexcept>
@@ -25,14 +26,24 @@ enum class CapitalType {
  */
 enum class BetStrategy {
     Vanilla,  ///< Vanilla betting strategy
-    Ada       ///< Adaptive betting strategy (to be implemented)
+    Ada       ///< Adaptive betting strategy
 };
 
 /**
- * @brief Type alias for vanilla betting function
+ * @brief Type erased factory function for any capital process
  */
-using VanillaBettingFn = std::function<std::pair<Float32, Int32>(
-    const Vector32f&, Float32, Float32, Int32, GeoCheckingCapital&)>;
+using AnyGamblerFactory = std::function<std::any(Float32, Float32, Int32)>;
+
+/**
+ * @brief Type erased betting function
+ */
+using AnyBettingFn = std::function<std::pair<Float32, Int32>(
+    const Vector32f&, Float32, Float32, Int32, std::any&)>;
+
+/**
+ * @brief Return type for betting factory
+ */
+using BettingFactoryResult = std::pair<AnyGamblerFactory, AnyBettingFn>;
 
 /**
  * @brief Factory function to create betting strategy and capital process.
@@ -45,38 +56,22 @@ using VanillaBettingFn = std::function<std::pair<Float32, Int32>(
  * @return A pair containing a factory function for the capital process and the betting function
  * 
  * @throws std::invalid_argument if unsupported strategy or capital type is requested
- * 
- * @note Currently only implements vanilla strategy. Adaptive strategy is TODO.
- * 
- * Example usage:
- * @code
- * auto [make_gambler, bet_fn] = betting_factory(BetStrategy::Vanilla, CapitalType::Geo);
- * auto gambler = make_gambler(0.5f, 0.1f);  // alpha, trunc_scale
- * auto result = bet_fn(samples, prior_mean, delta, grid_num, gambler);
- * @endcode
  */
-inline auto betting_factory(BetStrategy strategy, CapitalType capital) {
+inline BettingFactoryResult betting_factory(BetStrategy strategy, CapitalType capital) {
     switch (strategy) {
         case BetStrategy::Vanilla: {
             switch (capital) {
                 case CapitalType::Geo: {
-                    // Return factory for GeoCheckingCapital and vanilla betting
-                    auto make_gambler = [](Float32 alpha, Float32 trunc_scale, 
-                                          Int32 grid_num = 1000,
-                                          Float32 prior_mean = 0.5f,
-                                          Float32 prior_var = 0.25f,
-                                          Int32 num = 1,
-                                          Int32 sample_num = 100010) {
-                        return GeoCheckingCapital(alpha, trunc_scale, grid_num,
-                                                  prior_mean, prior_var, num,
-                                                  sample_num);
+                    AnyGamblerFactory make_gambler = [](Float32 alpha, Float32 trunc_scale, Int32 grid_num) -> std::any {
+                        return std::make_any<GeoCheckingCapital>(alpha, trunc_scale, grid_num);
                     };
                     
-                    VanillaBettingFn bet_fn = [](const Vector32f& samples,
-                                                  Float32 prior_mean,
-                                                  Float32 delta,
-                                                  Int32 grid_num,
-                                                  GeoCheckingCapital& gambler) {
+                    AnyBettingFn bet_fn = [](const Vector32f& samples,
+                                             Float32 prior_mean,
+                                             Float32 delta,
+                                             Int32 grid_num,
+                                             std::any& gambler_any) {
+                        auto& gambler = std::any_cast<GeoCheckingCapital&>(gambler_any);
                         return vanilla_betting(samples, prior_mean, delta, grid_num, gambler);
                     };
                     
@@ -84,31 +79,20 @@ inline auto betting_factory(BetStrategy strategy, CapitalType capital) {
                 }
                 
                 case CapitalType::Seq: {
-                    // TODO: Fix return type consistency - adaptive factories temporarily disabled
-                    throw std::invalid_argument("Sequence checking with vanilla betting not yet fully implemented");
-                    /*
-                    // Provide a factory and bet function for SequenceCheckingCapital
-                    auto make_gambler_seq = [](Float32 delta, Float32 trunc_scale,
-                                                Int32 grid_num = 1000,
-                                                Float32 prior_mean = 0.5f,
-                                                Float32 prior_var = 0.25f,
-                                                Int32 num = 1,
-                                                Int32 sample_num = 100100) {
-                        return SequenceCheckingCapital(delta, trunc_scale, grid_num,
-                                                       prior_mean, prior_var, num,
-                                                       sample_num);
+                    AnyGamblerFactory make_gambler = [](Float32 alpha, Float32 trunc_scale, Int32 grid_num) -> std::any {
+                        return std::make_any<SequenceCheckingCapital>(alpha, trunc_scale, grid_num);
                     };
-
-                    auto bet_fn_seq = [](const Vector32f& samples,
-                                          Float32 prior_mean,
-                                          Float32 delta,
-                                          Int32 grid_num,
-                                          SequenceCheckingCapital& gambler) {
+                    
+                    AnyBettingFn bet_fn = [](const Vector32f& samples,
+                                             Float32 prior_mean,
+                                             Float32 delta,
+                                             Int32 grid_num,
+                                             std::any& gambler_any) {
+                        auto& gambler = std::any_cast<SequenceCheckingCapital&>(gambler_any);
                         return vanilla_betting(samples, prior_mean, delta, grid_num, gambler);
                     };
-
-                    return std::make_pair(make_gambler_seq, bet_fn_seq);
-                    */
+                    
+                    return std::make_pair(make_gambler, bet_fn);
                 }
                 
                 default:
@@ -117,48 +101,44 @@ inline auto betting_factory(BetStrategy strategy, CapitalType capital) {
         }
         
         case BetStrategy::Ada: {
-            // TODO: Fix return type consistency - adaptive factories temporarily disabled
-            throw std::invalid_argument("Adaptive betting strategy factory not yet fully implemented (return type issues)");
-            /*
-            if (capital == CapitalType::Geo) {
-                using MakeGamblerFn = std::function<GeoCheckingCapital(Float32, Float32, Int32)>;
-                MakeGamblerFn make_gambler = [](Float32 alpha, Float32 trunc_scale, Int32 grid_num) {
-                    return GeoCheckingCapital(alpha, trunc_scale, grid_num);
-                };
+            switch (capital) {
+                case CapitalType::Geo: {
+                    AnyGamblerFactory make_gambler = [](Float32 alpha, Float32 trunc_scale, Int32 grid_num) -> std::any {
+                        return std::make_any<GeoCheckingCapital>(alpha, trunc_scale, grid_num);
+                    };
+                    
+                    AnyBettingFn bet_fn = [](const Vector32f& samples,
+                                             Float32 prior_mean,
+                                             Float32 delta,
+                                             Int32 grid_num,
+                                             std::any& gambler_any) {
+                        auto& gambler = std::any_cast<GeoCheckingCapital&>(gambler_any);
+                        return adaptive_betting(samples, prior_mean, delta, grid_num, gambler);
+                    };
+                    
+                    return std::make_pair(make_gambler, bet_fn);
+                }
                 
-                using AdaBettingFn = std::function<std::pair<Float32, Int32>(
-                    const Vector32f&, Float32, Float32, Int32, GeoCheckingCapital&)>;
+                case CapitalType::Seq: {
+                    AnyGamblerFactory make_gambler = [](Float32 alpha, Float32 trunc_scale, Int32 grid_num) -> std::any {
+                        return std::make_any<SequenceCheckingCapital>(alpha, trunc_scale, grid_num);
+                    };
+                    
+                    AnyBettingFn bet_fn = [](const Vector32f& samples,
+                                             Float32 prior_mean,
+                                             Float32 delta,
+                                             Int32 grid_num,
+                                             std::any& gambler_any) {
+                        auto& gambler = std::any_cast<SequenceCheckingCapital&>(gambler_any);
+                        return adaptive_betting(samples, prior_mean, delta, grid_num, gambler);
+                    };
+                    
+                    return std::make_pair(make_gambler, bet_fn);
+                }
                 
-                AdaBettingFn bet_fn = [](const Vector32f& samples,
-                                         Float32 prior_mean,
-                                         Float32 delta,
-                                         Int32 grid_num,
-                                         GeoCheckingCapital& gambler) {
-                    return adaptive_betting(samples, prior_mean, delta, grid_num, gambler);
-                };
-                
-                return std::make_pair(make_gambler, bet_fn);
-                
-            } else {
-                using MakeGamblerFn = std::function<SequenceCheckingCapital(Float32, Float32, Int32)>;
-                MakeGamblerFn make_gambler = [](Float32 alpha, Float32 trunc_scale, Int32 grid_num) {
-                    return SequenceCheckingCapital(alpha, trunc_scale, grid_num);
-                };
-                
-                using AdaBettingFn = std::function<std::pair<Float32, Int32>(
-                    const Vector32f&, Float32, Float32, Int32, SequenceCheckingCapital&)>;
-                
-                AdaBettingFn bet_fn = [](const Vector32f& samples,
-                                         Float32 prior_mean,
-                                         Float32 delta,
-                                         Int32 grid_num,
-                                         SequenceCheckingCapital& gambler) {
-                    return adaptive_betting(samples, prior_mean, delta, grid_num, gambler);
-                };
-                
-                return std::make_pair(make_gambler, bet_fn);
+                default:
+                    throw std::invalid_argument("Unsupported capital type");
             }
-            */
         }
         
         default:
@@ -171,7 +151,7 @@ inline auto betting_factory(BetStrategy strategy, CapitalType capital) {
  * 
  * @return Pair of (factory function, betting function)
  */
-inline auto vanilla_geo_factory() {
+inline BettingFactoryResult vanilla_geo_factory() {
     return betting_factory(BetStrategy::Vanilla, CapitalType::Geo);
 }
 
@@ -180,26 +160,8 @@ inline auto vanilla_geo_factory() {
  * 
  * @return Pair of (factory function, betting function)
  */
-inline auto vanilla_seq_factory() {
-    auto make_gambler_seq = [](Float32 delta, Float32 trunc_scale,
-                                Int32 grid_num = 1000,
-                                Float32 prior_mean = 0.5f,
-                                Float32 prior_var = 0.25f,
-                                Int32 num = 1,
-                                Int32 sample_num = 100100) {
-        return SequenceCheckingCapital(delta, trunc_scale, grid_num,
-                                       prior_mean, prior_var, num, sample_num);
-    };
-
-    auto bet_fn_seq = [](const Vector32f& samples,
-                          Float32 prior_mean,
-                          Float32 delta,
-                          Int32 grid_num,
-                          SequenceCheckingCapital& gambler) {
-        return vanilla_betting(samples, prior_mean, delta, grid_num, gambler);
-    };
-
-    return std::make_pair(make_gambler_seq, bet_fn_seq);
+inline BettingFactoryResult vanilla_seq_factory() {
+    return betting_factory(BetStrategy::Vanilla, CapitalType::Seq);
 }
 
 /**
@@ -207,7 +169,7 @@ inline auto vanilla_seq_factory() {
  * 
  * @return Pair of (factory function, betting function)
  */
-inline auto adaptive_geo_factory() {
+inline BettingFactoryResult adaptive_geo_factory() {
     return betting_factory(BetStrategy::Ada, CapitalType::Geo);
 }
 
@@ -216,7 +178,7 @@ inline auto adaptive_geo_factory() {
  * 
  * @return Pair of (factory function, betting function)
  */
-inline auto adaptive_seq_factory() {
+inline BettingFactoryResult adaptive_seq_factory() {
     return betting_factory(BetStrategy::Ada, CapitalType::Seq);
 }
 
